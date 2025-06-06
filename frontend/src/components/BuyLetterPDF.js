ea6f6bd2ac69beaf470980a8c7b93bf63ce87225
Labh Bothra <labhbothra1380@gmail.com>
2025-06-07 03:02:45 +0530

    DoneHHH

--- frontend/src/components/BuyLetterPDF.js
+++ frontend/src/components/BuyLetterPDF.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useContext } from "react";
+import React, { useState, useCallback, useContext,useEffect } from "react";
 import { PDFDocument, rgb, StandardFonts, degrees } from "pdf-lib";
 import { saveAs } from "file-saver";
 import {
