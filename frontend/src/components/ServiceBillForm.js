2333f57154879fdb0ace71b32ed9b1d7d0e60799
Labh Bothra <labhbothra1380@gmail.com>
2025-06-18 21:11:02 +0530

    Done

--- frontend/src/components/ServiceBillForm.js
+++ frontend/src/components/ServiceBillForm.js
@@ -436,7 +436,7 @@ const ServiceBillForm = () => {
           <img
             src={logo}
             alt="logo"
-            style={{ width: "7.5rem", height: "7.5rem", color: "#7c3aed" }}
+            style={{ width: "12.5rem", height: "7.5rem", color: "#7c3aed" }}
           />
           <p style={styles.sidebarSubtitle}>Welcome, OK MOTORS</p>
         </div>
