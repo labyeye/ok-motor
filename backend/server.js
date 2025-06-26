be1f345b09795110c130c80ee36b230823a012af
Labh Bothra <labhbothra1380@gmail.com>
2025-06-26 22:49:05 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const buyLetterRoutes = require('./routes/buyLetter');
-const sellLetterRoutes = require('./routes/selLetter');
-const dashboardRoutes = require('./routes/dashboard');
-const serviceBillRoutes = require('./routes/serviceBillRoutes');
-const advanceBillRoutes = require('./routes/advanceBillRoutes');
-const { protect } = require('./middleware/auth');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const buyLetterRoutes = require("./routes/buyLetter");
+const sellLetterRoutes = require("./routes/selLetter");
+const dashboardRoutes = require("./routes/dashboard");
+const serviceBillRoutes = require("./routes/serviceBillRoutes");
+const advanceBillRoutes = require("./routes/advanceBillRoutes");
+const { protect } = require("./middleware/auth");
+const cors = require("cors");
 const app = express();
 connectDB();
 const corsOptions = {
-  origin: 'https://ok-motor.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "https://ok-motor.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 app.use(express.json());
 app.use(cors(corsOptions));
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/users', protect, userRoutes);
-app.use('/api/buy-letter', buyLetterRoutes);
-app.use('/api/sell-letters',sellLetterRoutes);
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/service-bills', serviceBillRoutes);
-app.use('/api/advance-bills', advanceBillRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", protect, userRoutes);
+app.use("/api/buy-letter", buyLetterRoutes);
+app.use("/api/sell-letters", sellLetterRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/service-bills", serviceBillRoutes);
+app.use("/api/advance-bills", advanceBillRoutes);
 
 const PORT = 2500;
 
